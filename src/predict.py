699917a73ed7d6aa7379ef927b496aa391e275b8
Guillermo112 <Guillermo.mujica@hotmail.com>
2024-09-10 02:17:10 +0000

    Make_dataset y Predict

--- src/predict.py
+++ src/predict.py
@@ -1,32 +1,48 @@
-# Código de Scoring - Modelo de Riesgo de Default en un Banco de Corea
-############################################################################
+###################################
+# Sistema de Recomendacion (predict.py)
+###################################
 
-import pandas as pd
-import xgboost as xgb
-import pickle
-import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+# Import linear_kernel
+from sklearn.metrics.pairwise import linear_kernel
 
+# Stopwords
 
-# Cargar la tabla transformada
-def score_model(filename, scores):
-    df = pd.read_csv(os.path.join('../data/processed', filename)).set_index('ID')
-    print(filename, ' cargado correctamente')
-    # Leemos el modelo entrenado para usarlo
-    package = '../models/best_model.pkl'
-    model = pickle.load(open(package, 'rb'))
-    print('Modelo importado correctamente')
-    # Predecimos sobre el set de datos de Scoring    
-    res = model.predict(df).reshape(-1,1)
-    pred = pd.DataFrame(res, columns=['PREDICT'])
-    pred.to_csv(os.path.join('../data/scores/', scores))
-    print(scores, 'exportado correctamente en la carpeta scores')
+def matrix_stopwords(df, feature):
+	tfidf = TfidfVectorizer(stop_words='english')
+        #Replace NaN with an empty string
+	df[feature] = df[feature].fillna('')
+	#Construct the required TF-IDF matrix by fitting and transforming the data
+	tfidf_matrix = tfidf.fit_transform(df[feature])
+	return tfidf_matrix
+ 
+# Compute the cosine similarity matrix
+def matrix_similarity(matrix):
+	cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    indices = pd.Series(netflix_overall.index, index=netflix_overall['title']).drop_duplicates()
 
+#Sistema de Recomendacion
+def get_recommendations(title, cosine_sim=cosine_sim):
+    idx = indices[title]
 
-# Scoring desde el inicio
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return netflix_overall['title'].iloc[movie_indices]
+
 def main():
-    df = score_model('credit_score.csv','final_score.csv')
-    print('Finalizó el Scoring del Modelo')
-
+	tfidf_matrix = matrix_stopwords('netflix_train.csv', ['description'])	
 
 if __name__ == "__main__":
     main()
